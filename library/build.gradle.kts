baaebd97662a15970dc8ecc23563ce5cff30715a
ikhomutskikh <homutskih@gmail.com>
2026-06-10 12:12:22 +0200

    chore: configure Maven Central publication with vanniktech plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.vanniktech)
+    id("signing")
 }
 
 kotlin {
@@ -62,10 +66,61 @@ android {
     }
 }
 
+val localProps = gradleLocalProperties(rootDir, providers)
+
+group = "io.github.froyder"
+version = "1.0.0"
+
+signing {
+    val signingKeyId = localProps.getProperty("signing.keyId") ?: ""
+    val signingPassword = localProps.getProperty("signing.password") ?: ""
+    val signingSecretKeyFile = localProps.getProperty("signing.secretKeyFile") ?: ""
+
+    if (signingKeyId.isNotEmpty() && signingSecretKeyFile.isNotEmpty()) {
+        signing {
+            useInMemoryPgpKeys(
+                signingKeyId,
+                file(signingSecretKeyFile).readText(),
+                signingPassword
+            )
+        }
+    }
+}
+
 mavenPublishing {
     coordinates(
         groupId = "io.github.froyder",
         artifactId = "kmp-inapp-review",
-        version = "0.1.0-local"
+        version = "1.0.0"
     )
+
+    pom {
+        name.set("KMP In-App Review")
+        description.set("A Kotlin Multiplatform library that wraps Google Play In-App Review and SKStoreReviewController under a unified coroutine-friendly API.")
+        url.set("https://github.com/Froyder/kmp-inapp-review")
+
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("froyder")
+                name.set("Ilia Khomutskikh")
+                email.set("homutskih@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Froyder/kmp-inapp-review")
+            connection.set("scm:git:git://github.com/Froyder/kmp-inapp-review.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Froyder/kmp-inapp-review.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
